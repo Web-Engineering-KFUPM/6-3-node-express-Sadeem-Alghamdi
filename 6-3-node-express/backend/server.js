310629deb77f91794cb71bdc6c191faf7a5c8f9e
Sadeem-Alghamdi <s202244580@kfupm.edu.sa>
2026-04-06 11:12:15 +0300

    1

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,6 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+const app = express();
 
 // TODO 1: Define server port
-
-
-// TODO 4: Import and Apply CORS middleware
-
-
-// TODO 5: Import and apply Morgan middleware
-
-
-// TODO 6.1: Create root route "/"
-
-
-// TODO 6.2: Create "/api/quote" route
-
-
-// TODO 7: Start server using app.listen
+const PORT = 3000;
